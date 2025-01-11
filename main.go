a8bcede579b1b8d641fece51d4293adc1cf30a2c
diptesh <dipteshmandal555@gmail.com>
2025-01-11 23:40:30 +0530

    added new peer

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/diptesh/filestore/p2p"
+)
+
+func main() {
+	// Create a new TCP transport instance
+	tr := p2p.NewTCPTransport(":3000")
+
+	// Start listening and accepting connections
+	err := tr.ListenAndAccept()
+	if err != nil {
+		log.Fatal(err)
+		fmt.Printf("Error starting transport: %s\n", err)
+		return
+	}
+
+	// Keep the program running to accept incoming connections
+	select {}
+}
--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -6,11 +6,29 @@ import (
 	"sync"
 )
 
+type TCPPeer struct {
+	conn     net.Conn
+	outbount bool
+}
+
+// // Add a String method to TCPPeer
+// func (p *TCPPeer) String() string {
+// 	return p.conn.RemoteAddr().String() // This will print the remote address of the connection
+// }
+
 type TCPTransport struct {
 	listnerAdder string
 	listner      net.Listener
 	mu           sync.RWMutex
 	peers        map[net.Addr]Peer
+}
+
+func NewTCPPeer(conn net.Conn, outbount bool) *TCPPeer {
+
+	return &TCPPeer{
+		conn:     conn,
+		outbount: outbount,
+	}
 }
 
 // NewTCPTransport creates a new TCPTransport instance.
@@ -19,6 +37,7 @@ func NewTCPTransport(listnerAddr string) *TCPTransport {
 	return &TCPTransport{
 		listnerAdder: listnerAddr,
 		peers:        make(map[net.Addr]Peer),
+		mu:           sync.RWMutex{},
 	}
 }
 func (t *TCPTransport) ListenAndAccept() error {
@@ -34,15 +53,19 @@ func (t *TCPTransport) ListenAndAccept() error {
 
 func (t *TCPTransport) StartAcceptingLoop() error {
 	for {
+		// fmt.Println("Accepted a new connection")
 		conn, err := t.listner.Accept()
+		// fmt.Println("Accepted a new connection")
 		if err != nil {
 			fmt.Printf("TCP accept error: %s\n", err)
-			return err
+			// return err
 		}
+		// fmt.Println("Accepted a new connection")
 		go t.HandleConnection(conn)
 
 	}
 }
 func (t *TCPTransport) HandleConnection(conn net.Conn) {
-	fmt.Printf("New incoming connection from %s\n", conn.RemoteAddr())
+	peer := NewTCPPeer(conn, true)
+	fmt.Printf("New incoming connection from %v\n", peer)
 }
